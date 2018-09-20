5a22ca2a481ac7cc4ec1807a99cb4a910d2cae79
Scott Guthrie <jsguthrie@nc.rr.com>
2018-09-20 18:20:35 -0400

    start on GAS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
+import { Container } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
 
 import Home from './components/home/Home';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
+import { VehicleLookup } from "./vehicle-lookup/VehicleLookup";
+import { UsedValues } from "./used-values/UsedValues";
 
 // class component
 class App extends Component {
@@ -18,21 +19,24 @@ class App extends Component {
       <div className="App">
         <Header></Header>
         <section>
-          <Route render={({ location }) => (
-            <TransitionGroup>
-              <CSSTransition
-                key={location.key}
-                timeout={300}
-                classNames="slide"
-              >
-                <Switch location={location}>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/about" component={About} />
-                  <Route path="/contact" component={Contact} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )} />
+          <Container fluid >
+            <h2 className="mt-4 mb-3">Used Values</h2>
+            <Route render={({ location }) => (
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  timeout={300}
+                  classNames="slide"
+                >
+                  <Switch location={location}>
+                    <Route exact={true} path="/" component={VehicleLookup} />
+                    <Route path="/vehicle-lookup" component={VehicleLookup} />
+                    <Route path="/used-values" component={UsedValues} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )} />
+          </Container>
         </section>
         <Footer></Footer>
       </div>
